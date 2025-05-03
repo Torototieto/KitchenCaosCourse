b558c2fc765fb0871adbc63a147532481f1c8bf2
Andrés Holguera <andres@holguera.org>
2025-05-03 07:56:32 +0200

    Refactor movement and change input system

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        // To avoid the player from moving faster diagonally we need to
+        // normalize the Vector2
+        inputVector.Normalize();
+
+        Debug.Log(inputVector);
+
+        return inputVector;
+    }
+}

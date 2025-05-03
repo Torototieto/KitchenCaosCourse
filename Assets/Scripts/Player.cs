64d9468128c888984fb47aa27fb44f5663ec388c
Andrés Holguera <andres@holguera.org>
2025-05-03 07:56:20 +0200

    Refactor movement and change input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,33 +4,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
    [SerializeField] private float moveSpeed = 7f;
+   [SerializeField] private GameInput gameInput;
 
    private bool isWalking;
 
    // Update is called once per frame
    private void Update()
    {
-      Vector2 inputVector = new Vector2(0, 0);
-      if (Input.GetKey(KeyCode.W))
-      {
-         inputVector.y = +1;
-      }
-      if (Input.GetKey(KeyCode.S))
-      {
-         inputVector.y = -1;
-      }
-      if (Input.GetKey(KeyCode.A))
-      {
-         inputVector.x = -1;
-      }
-      if (Input.GetKey(KeyCode.D))
-      {
-         inputVector.x = +1;
-      }
-
-      // To avoid the player from moving faster diagonally we need to
-      // normalize the Vector2
-      inputVector.Normalize();
+      Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
       // Since inputVector is a Vector2, we need to convert it to a 
       // Vector3 so we can move on the right axis

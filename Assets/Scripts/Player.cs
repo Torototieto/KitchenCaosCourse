0e3295588201b3784ea94a77e644e6655a144eda
Andrés Holguera <andres@holguera.org>
2025-05-06 20:07:17 +0200

    Input Interact and Event Interact

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,33 @@ public class Player : MonoBehaviour
    private bool isWalking;
    private Vector3 lastInteractDir;
 
+   private void Start()
+   {
+      gameInput.OnInteractAction += GameInput_OnInteractAction;
+   }
+
+   private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+   {
+      Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+      Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+      float interactDistance = 2f;
+
+      if (moveDir != Vector3.zero)
+      {
+         lastInteractDir = moveDir;
+      }
+
+      // Uses and overload method that returns the object hit (out RaycastHit)
+      if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+      {
+         if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+         {
+            // Has ClearCounter
+            clearCounter.Interact();
+         }
+      }
+   }
+
    // Update is called once per frame
    private void Update()
    {
@@ -86,7 +113,7 @@ public class Player : MonoBehaviour
    private void HandleInteractions()
    {
       Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-      Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); 
+      Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
       float interactDistance = 2f;
 
       if (moveDir != Vector3.zero)
@@ -100,8 +127,7 @@ public class Player : MonoBehaviour
          if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
          {
             // Has ClearCounter
-            clearCounter.Interact();
          }
-      } 
+      }
    }
 }
